c0de7e5bfb201ba57bbe282411a4ce573243d2fd
Koyejo <adinlewakoyejo@yahoo.com>
2023-08-15 23:55:35 -0700

    Commented out print statements in do_quit & do_EOF

--- console.py
+++ console.py
@@ -5,21 +5,21 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """Command line interpreter to allow interactive program usage"""
-    intro = '\033[1;32mWelcome to the HBNB shell.\
-             \nType \'help\' or \'?\' to see command options.'
+    # intro = '\033[1;32mWelcome to the HBNB shell.\
+    #         \nType \'help\' or \'?\' to see command options.'
     prompt = '\033[1;37;40m(hbnb) '
 
     def do_quit(self, line):
         """Type command to exit the program\n"""
-        print('You\'re exiting the shell...')
-        print('Bye!!!')
+        # print('You\'re exiting the shell...')
+        # print('Bye!!!')
         return True
 
     def do_EOF(self, line):
         """Command to exit the program\n"""
-        print('\033[1;31;40m\nYou\'re exiting the shell...')
-        print('Bye!!!')
-        print('\033[1;37;40m')
+        # print('\033[1;31;40m\nYou\'re exiting the shell...')
+        # print('Bye!!!')
+        # print('\033[1;37;40m')
         return True
 
     def emptyline(self):
