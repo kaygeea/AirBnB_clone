7a2ea76b962f10977c3d30150e373adfca1c8f61
Koyejo Adinlewa <adinlewakoyejo@yahoo.com>
2024-08-08 21:44:53 +0100

    Add do_create method to handle creation of BaseModel instance

--- console.py
+++ console.py
@@ -12,7 +12,8 @@ Classes:
 Usage:
     Run this script directly to start the command interpreter.
 """
-
+from models.base_model import BaseModel
+from models import storage
 import cmd
 
 class HBNBCommand(cmd.Cmd):
@@ -34,15 +35,53 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         """Quit command to exit the program.\n"""
+        print("Quitting...")
         return True
 
     def do_EOF(self, arg):
         """EOF command to exit the program."""
+        print("EOF received. Exiting...")
         return True
 
     def emptyline(self):
         """Do nothing on empty input line."""
         pass
+
+    def do_create(self, arg: str):
+        """
+        Create a new instance of a specified class, save it to storage, 
+        and print its `id`.
+        
+        Usage: 
+            `create BaseModel`
+        
+        This command supports creating instances of the `BaseModel` class.
+        
+        - If the class name is missing, prints: ** class name missing **
+        - If the class name doesn't exist, prints: ** class doesn't exist **
+        - If the class name is recognized, the new instance is created, 
+        saved to the storage system, and its `id` is printed.
+        
+        Args:
+            arg (str): The name of the class to instantiate.
+        
+        """
+        args = arg.split()
+        num_args = len(args)
+        if not arg:
+            print("** class name missing **")
+            return
+        if num_args > 1:
+            print("** this method takes only 1 positional argument **")
+            return
+        elif arg and num_args == 1 and args[0] == BaseModel.__name__:
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
+            return
+        else:
+            print("** class doesn't exist **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
